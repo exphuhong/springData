d154c7ea9e4f8a33085297a4cdbd03a5d1b64d33
CS_HH <1042331526@qq.com>
2017-11-27 15:14:45 +0800

    配置好dataSource

---
+++ src/main/java/com/IFox/springData/SpringDataTest.java
@@ -0,0 +1,30 @@
+package com.IFox.springData;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by exphuhong
+ * Date 17-11-27.
+ * Start
+ */
+public class SpringDataTest {
+    private ApplicationContext context = null;
+    {
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    }
+
+    @Test
+    public void test() {
+        DataSource dataSource =  context.getBean(DataSource.class);
+        try {
+            System.out.println(dataSource.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
